a7d5878fcc6af8c741d872a28b2d295fbeb35a19
unknown <rc.sanoiz@gmail.com>
2026-03-03 18:44:49 +0800

    continued working on the base code

--- main.py
+++ main.py
@@ -11,7 +11,49 @@ def play(word):
     guessed_letters = []
     guessed_words = []
     tries = 6
-
+    print("Welcome to the Hangman Game!")
+    print(display_hangman(tries))
+    print(word_completion)
+    print("\n")
+    while not guessed and tries > 0:
+        guess = input("Please guess a letter or word:").upper()
+        if len(guess) == 1 and guess.isalpha():
+            if guess in guessed_letters:
+                print("You already guessed the letter", guess)
+            elif guess not in word:
+                print(guess, "is not in the word.")
+                tries -= 1
+                guessed_letters.append(guess)
+            else:
+                print("Good job,", guess, "is in the word!")
+                guessed_letters.append(guess)
+                word_as_list = list(word_completion)
+                indices = [i for i, letter in enumerate(word) if letter == guess]
+                for index in indices:
+                    word_as_list[index] = guess
+                word_completion = "".join(word_as_list)
+                if "_" not in word_completion:
+                    guessed = True
+        elif len(guess) == len(word) and guess.isalpha():
+            if guess in guessed_words:
+                print("You already guessed the word", guess)
+            elif guess != word:
+                print(guess, "is not in the word.")
+                tries -= 1
+                guessed_words.append(guess)
+            else:
+                guessed = True
+                word_completion = word
+        else:
+            print("Not a valid guess.")
+        print(display_hangman(tries))
+        print(word_completion)
+        print("\n")
+    if guessed:
+        print("Congratulations, you guessed the word! Malupet!")
+    else:
+        print("Sorry, you ran out of guesses. The word was " + word + ". Next time, pare!")
+        
 def display_hangman(tries):
     stages = [  # final state: head, torso, both arms, and both legs
                 """
